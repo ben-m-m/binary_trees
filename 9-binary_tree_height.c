9a975f6410d8184b9c24db0464743f8c9f56e972
ben-m-m <mainaben23@gmail.com>
2023-06-02 11:10:49 +0300

    height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
