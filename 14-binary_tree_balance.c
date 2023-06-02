adfda32ee8f695917ae7c79fc4da7e8320aa7322
ben-m-m <mainaben23@gmail.com>
2023-06-02 18:49:38 +0300

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,21 +6,22 @@
  * Return: tree height
  */
 
-int binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
 		return (right_height + 1);
 }
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
